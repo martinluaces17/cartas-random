51846a3c75fb6a9fc89173e6159d53116b283752
Valentina Cuello <delmar_vale08@hotmail.com>
2021-04-26 13:43:50 +0000

    ejercicio sin los comentarios

--- src/app.js
+++ src/app.js
@@ -34,72 +34,88 @@ function generateRandomPalo() {
 
 //Función que crea la carta en HTML
 function dibujarCarta(palo, numero) {
-  let carta = document.createElement("div"); //acá creo el elemento div
-  carta.classList.add("card"); //le agrego la clase .card
+  let carta = document.createElement("div");
+  carta.classList.add("card");
 
-  let contenido = document.createElement("span"); //creo una variable para el contenido. creo el elemento span
-  contenido.classList.add("number", palo); //le agrego la clase .number y el parámetro palo
-  contenido.innerHTML = numero; //mediante el innerHTML le agrego al span el parámetro número
+  let contenido = document.createElement("span");
+  contenido.classList.add("number", palo);
+  contenido.innerHTML = numero;
 
-  carta.appendChild(contenido); //acá lo que hago es al elemento carta mediante appendChild le agrego el elemento contenido como hijo (que será el contenido de la carta)
-  return carta; //retorno la carta (con todo: también con su hijo "contenido")
+  carta.appendChild(contenido);
+  return carta;
 }
 
 var listaCartas = [];
 
-//Esta función es para generar las cartas dependiendo del valor que se le ingrese en el input
 function generadorDeCartas() {
-  var inputValue = document.getElementById("input").value; //esta variable es para "agarrar" el valor del input
+  listaCartas = [];
+  var inputValue = document.getElementById("input").value;
 
-  ///al iniciar por primera vez la función la variable containerCartas va a dar null
-  // en una próxima vez que se ejecute la función al apretar el botón, ese conteiner-card sí va a existir, entonces
-  //la variable containerCartas ya no va a ser null y mediante el querySelector la está llamando y ahí se ejecutaría el if
-  //de la siguiente línea por lo tanto
-
-  let containerCartas = document.querySelector(".container-card"); //esta variable llama a elemento con clase .container-card
+  let containerCartas = document.querySelector(".container-card");
   if (containerCartas != null) {
-    //con el if decimos que si containerCartas es distinto de null (es decir, tiene contenido, no está vacío)
-    document.querySelector(".padre").removeChild(containerCartas); //entonces llamando al .padre, remueve al hijo containerCartas (es decir, el contenedor que tiene todas las cartas)
+    document.querySelector(".padre").removeChild(containerCartas);
   }
 
-  let newContainerCartas = document.createElement("div"); //entonces acá creamos una variable que creará al div
-  newContainerCartas.classList.add("container-card"); //a ese div acá le agregamos la clase .container-card
-  let padre = document.querySelector(".padre"); //acá traemos desde el html al div .padre
-  padre.appendChild(newContainerCartas); //y acá le agregamos en forma de child -hijo- la variable anterior que contiene el container-card
+  let newContainerCartas = document.createElement("div");
+  newContainerCartas.classList.add("container-card");
+  let padre = document.querySelector(".padre");
+  padre.appendChild(newContainerCartas);
 
   for (var i = 0; i < inputValue; i++) {
-    //iniciamos el bucle donde el condicional es dependiendo del valor del input
-    let palo = generateRandomPalo(); //creamos esta variable que es igualada a la función que genera de forma random el palo
-    let numero = generateRandomNumber(); //acá lo mismo pero con el número
+    let palo = generateRandomPalo();
+    let numero = generateRandomNumber();
 
-    let cartaGenerada = dibujarCarta(palo, numero); //en este linea creamos una variable que sería la carta generada donde su valor es la función que "arma" el código html de la carta: fondo, número y palo
-    //y le pasamos como parametros las variables palo y numero que son quienes generarn de forma random esos numeros y figuras
-    newContainerCartas.appendChild(cartaGenerada); //luego al newContainerCartas (que sería el contenedor de todas las cartas) le colocamos como hijo la carta generada que está guardada en la variable cartaGenerada
+    let cartaGenerada = dibujarCarta(palo, numero);
+    newContainerCartas.appendChild(cartaGenerada);
 
-    listaCartas.push(numero);
+    listaCartas.push(cartaGenerada);
     console.log(listaCartas);
   }
 }
+
+const bubbleSort = arr => {
+  console.log(arr);
+  let wall = arr.length - 1; //we start the wall at the end of the array
+  while (wall > 0) {
+    let index = 0;
+    while (index < wall) {
+      //compare the adjacent positions, if the right one is bigger, we have to swap
+
+      if (arr[index] > arr[index + 1]) {
+        let aux = arr[index];
+        arr[index] = arr[index + 1];
+        arr[index + 1] = aux;
+      }
+      index++;
+    }
+    wall--; //decrease the wall for optimization
+  }
+
+  return arr;
+};
 
 function limpiarInput() {
   let inputValue = (document.getElementById("input").value = " ");
 }
 
+function ordenar() {
+  let cartasOrdenadas = bubbleSort(listaCartas);
+}
+
 window.onload = function() {
-  //genera las cartas
   var botonGenerador = document.getElementById("draw");
   botonGenerador.addEventListener("click", generadorDeCartas);
 
-  //las borra
   var btnClear = document.getElementById("clear");
   btnClear.addEventListener("click", function() {
-    //creo esta variable donde llamo mediante queryselector al elemento que contenga la clase .container-card
     let containerCartas = document.querySelector(".container-card");
     if (containerCartas != null) {
-      //con este if decimos que si esa variable es distinta de null, es decir, tiene contenido
-      document.querySelector(".padre").removeChild(containerCartas); //se llama desde el html al padre y se le remueve el hijo container-cards
+      document.querySelector(".padre").removeChild(containerCartas);
     }
     limpiarInput();
-    listaCartas = []; //esto en teoría borra el array, es decir, por cada vez que se generan nuevas cartas y luego se borran, el array vuelve a estar vacío
+    listaCartas = [];
   });
+
+  let btnOrdenar = document.getElementById("ordenar");
+  btnOrdenar.addEventListener("click", ordenar);
 };
